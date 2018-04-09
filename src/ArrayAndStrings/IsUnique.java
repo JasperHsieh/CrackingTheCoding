ce4a6e1dee6ef76748b00217d123bb01c6e12b88
Jasper Hsieh <jasperhsieh666@gmail.com>
2018-04-09 22:02:36 +0800

    Add log for val

--- src/ArrayAndStrings/IsUnique.java
+++ src/ArrayAndStrings/IsUnique.java
@@ -12,6 +12,11 @@ public class IsUnique {
         int checker = 0;
         for (int i = 0; i < str.length(); i++) {
             int val = str.charAt(i) - 'a';
+            System.out.println("val    =" + val);
+            System.out.println("val    =" + Integer.toBinaryString(val));
+            System.out.println("checker=" + Integer.toBinaryString(checker));
+            System.out.println("1<<val =" + Integer.toBinaryString(1 << val));
+            System.out.println("");
             if ((checker & (1 << val)) > 0) {
                 return false;
             }
